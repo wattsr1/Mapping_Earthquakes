4593e9833ea4356fa9335715a5801f9571691aaf
Roger Watts <wattsr1@hotmail.com>
2022-01-22 11:13:46 -0500

    Changed tectonic link to repository

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -212,7 +212,7 @@ legend.onAdd = function() {
 
 
 //  // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json("GeoJSON/PB2002_boundaries.json").then(function(data) {
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(function(data) {
     
     L.geoJSON(data, {
       style: {
